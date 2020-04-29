52cd6ff16fac0b526efb566c6727967ee43ccaf1
Augusto Giacomini <augustocesartg@gmail.com>
2020-04-28 22:53:03 -0300

    commit

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,6 +1,6 @@
 const formatValue = (value: number): string =>
   Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
-    value,
+    value || 0,
   );
 
 export default formatValue;
